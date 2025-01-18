8f6d91cccd6b3a24c893dd3e311b4a02dd78dfa9
Sandeep Kumar <sandeep.kumar_84@yahoo.co.in>
2025-01-18 14:15:02 +0000

    v8

---
+++ 08vitereact/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    
+      <div style={{padding:10}}>
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+      </div>
+    </>
+  )
+}
+
+export default App
