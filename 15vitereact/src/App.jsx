819f5f8eb3c386755cd44398b0c07f5510b68e68
Sandeep Kumar <sandeep.kumar_84@yahoo.co.in>
2025-09-17 15:52:52 +0100

    practice

--- 15vitereact/src/App.jsx
+++ 15vitereact/src/App.jsx
@@ -1,33 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
+import { useState } from 'react'
 function App() {
-  const [count, setCount] = useState(0)
+  const [color, setColor] = useState('lightgrey')
 
   return (
     <>
+    <div style={{backgroundColor:color}}>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <table>
+          <tbody>
+          <tr>
+            <td>
+              <button id="blue" style={{padding:10}} onClick={()=>{setColor('blue')}}>Blue</button>
+            </td>
+            <td>
+              <button id="red" style={{padding:10}} >Red</button>
+            </td>
+            <td>
+              <button id="green" style={{padding:10}}>Green</button>
+            </td>        
+          </tr>      
+          </tbody>
+        </table>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    </div>
+    
+
     </>
   )
 }
