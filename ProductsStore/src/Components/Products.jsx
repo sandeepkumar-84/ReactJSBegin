abb007b36ee8785ef5268daa92a9d7078166af13
Sandeep Kumar <sandeep.kumar_84@yahoo.co.in>
2025-12-03 19:29:09 +0000

    Complete put post delete and get methos implementation

--- ProductsStore/src/Components/Products.jsx
+++ ProductsStore/src/Components/Products.jsx
@@ -6,6 +6,8 @@ function Products()
     const {dataProds,refresh} = useProductsApi(); 
 
     const [ProductName,SetProductName] = useState("");
+    const [editid,SetEditId] = useState(null);
+    const [editName, setEditName] = useState("");
    
 
     const handleAddProduct = async ()=>{
@@ -32,7 +34,55 @@ function Products()
         }
     };
 
-    
+    const handleEditClick = (product)=>
+    {
+           SetEditId(product.id);
+           setEditName(product.name);
+    }
+    const handleUpdate = async () => {
+         alert("handleUpdateClick");
+        if (editName.trim() === "") {
+            alert("Name cannot be empty");
+            return;
+        }
+
+        const response = await fetch(`https://localhost:7029/api/products/${editid}`, {
+            method: "PUT",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ id: editid, name: editName })
+        });
+
+        if (response.ok) {
+            SetEditId(null);
+            setEditName("");
+            refresh();
+        } else {
+            alert("Update failed");
+        }
+    };
+
+    const handleDelete = async (id) =>
+        {
+              alert("Delete Clicked");
+
+            const response = await fetch(`https://localhost:7029/api/products/${id}`,
+
+                {
+                    "method":"DELETE",
+                    "header":{"content-type":"application/json"}
+                }
+
+            )
+
+            if(response.ok)
+            {
+                refresh();    
+            }
+            else
+            {
+                alert("Delete failed");
+            }
+        }
 
     return(
         <>
@@ -55,9 +105,23 @@ function Products()
                     products => 
                     (
                        
-                            <tr><td style={{ border: "1px solid #ccc" }}><label> {products.name}</label></td></tr>  
+                            //<tr><td style={{ border: "1px solid #ccc" }}><label> {products.name}</label></td></tr>  
+                            <tr key = {products.id}>
+                                <td style={{ border: "1px solid #ccc", padding:"8px" }}> 
+                                    {
+                                        editid == products.id ? (<input type="text" value={editName} onChange={(e) => setEditName(e.target.value)}/>):( <label>{products.name}</label>)
+                                    }
+                                </td>
+                                <td>
+                                    {
+                                        editid == products.id ? (<button onClick={handleUpdate}>Save</button>):(<button onClick={()=>{handleEditClick(products)}}>Edit</button>)
+                                    }
+                                </td>
+                                <td>
+                                        <button onClick={()=>{handleDelete(products.id)}}>Delete</button>
+                                </td>                                
+                            </tr>                         
                     ))
-
                 }
                 </tbody>
             </table>
